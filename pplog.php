52c899f733a657b8b5e5b5a7ca26cefd03e6ae68
SniperD33 <111898994+SniperD33@users.noreply.github.com>
2023-04-25 10:34:37 -0700

    Add files via upload
    updated and now loads to the page and gives the opportunity for growth

--- pplog.php
+++ pplog.php
@@ -10,19 +10,37 @@
 		<h1>Plant Page</h1>
 	</div>
 	<div id="navigation">
-		<a href="plog.html">Plant Dictionary</a>
-		<a href="title.html">Brought to You by</a>
-		<a href="home.html">Home</a>
+		<a href="plog.php">Plant Dictionary</a>
+		<a href="title.php">Brought to You by</a>
+		<a href="home.php">Home</a>
 	</div>
 	<div id="content">
 		<div class="personalPlant">
-			<?php foreach($plants as $key): ?>
-			Name: {$key['']['']}<br>
-			Type: {$key['']['']}<br>
-			Difficulty Level: {$key['']['']}<br>
-			Description: {$key['']['']}<br>
+<?php
+foreach($plants as $key){
+	echo<<<PLANT
+	<div id='content'>
+		<div class='personalPlant'>
+		Name: {$key['PersonalPlant']['PPName']}<br>
+		Name: {$key['PersonalPlant']['SciName']}<br>
+		Home Environment: {$key['PersonalPlant']['HomeEnv']}<br>
 		</div>
 	</div>
+	PLANT;
+	}
+?>
 	<footer> <p> Plant Page</p></footer>
 </body>
 </html>
+/* image would need to be added to each of the plants, 
+ * and added to the rosters
+ * would be put back in the onStart section
+ * <img src='{$key['PersonalPlant']['image']} id= 'allImages'><br>
+ *
+ * difficulty level and discription would also need to be added to the
+ * roster and each of the plants 
+ * both would be put back in the whenClicked section
+ *
+ * Difficulty Level: {$key['PersonalPlant']['']}<br>
+ * Description: {$key['PersonalPlant']['']}<br>
+*/
