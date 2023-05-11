893e20827233d10e82a698952a5b7c4f9937d9ab
SniperD33 <111898994+SniperD33@users.noreply.github.com>
2023-05-11 13:14:40 -0700

    finished

--- home.php
+++ home.php
@@ -1,26 +1,53 @@
+<?php 
+require_once ("config.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Plants Page</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <title>Plants Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div id="header">
-		<h1>Plant Page</h1>
-	</div>
-	<div id="navigation">
-		<a href="plog.html">Plant Dictionary</a>
-		<a href="pplog.html">Personal Plant Log</a>
-		<a href="title.html">Brought to You by</a>
-	</div>
-	<div id="content">
+    <div id="header">
+        <h1>Plant Page</h1>
+    </div>
+    <div id="navigation">
+        <a href="plog.php">Plant Dictionary</a>
+        <a href="pplog.php">Personal Plant Log</a>
+        <a href="title.php">Brought to You by</a>
+    </div>
+    <div id="content">
+    <div id = "PoD">
+    <h2>Plant of the Day</h2>
+<?php
 
-	</div>
+$db = get_mysqli_connection();
+$query = $db->prepare("SELECT * FROM POD");
+$query->execute();
 
+$result = $query->get_result();
+$rows = $result->fetch_all(MYSQLI_ASSOC);
 
+echo makeTable($rows);
+?>
+    </div>
+<div id= "MVP">
+<h2> Most Viewed Plant</h2>
+<?php 
 
+$db = get_mysqli_connection();
+$query = $db->prepare("SELECT * FROM TopViewPlants");
+$query->execute();
 
-	<footer> <p> Plant Page</p></footer>
+$result = $query->get_result();
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+
+echo makeTable($rows);
+
+?>
+        </div>
+    </div>
+    <footer> <p> Plant Page</p></footer>
 </body>
 </html>
